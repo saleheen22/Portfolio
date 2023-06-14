702652f8f60361a7d6d6d52cd235169dfc672039
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2023-06-14 21:24:05 +0600

    project ssetup done with tailwind css and daisy ui

---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">This is home page</h2>
+            <button className="btn btn-warning">Button</button>
+        </div>
+    );
+};
+
+export default Home;
