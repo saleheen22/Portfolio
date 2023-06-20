45e28591fb9e37cba4f04046746d1893a17b6707
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2023-06-21 02:21:03 +0600

    Reamde file added with info

---
+++ README.md
@@ -0,0 +1,14 @@
+## The live link
+[Portfolio Website](https://abrar-saleheen.web.app/ "Abrar Saleheen portfolio website live link").
+
+### Notable Packages Used:
+> React tsparticles (Banner section's background),
+> React keen-slider (Projects),
+> React typewrite(Banner)
+> Email Js for Message section
+> React tabs (Resume section)
+### Key Features:
+* It is a front-end only web projects build with React. 
+* Tailwind CSS and component library daisy Ui used 
+* There are only three main section: About me, Resume and       message. In the resume 4 tabs are integrated for showcasing the skills, projects, education and extracurricular.
+* Any visitor can send a message in through the website. The message me section is implemented with Email Js. The message will be delivered to My email address. 
--- src/components/About/Social/SocialLink.jsx
+++ src/components/About/Social/SocialLink.jsx
@@ -6,9 +6,9 @@ const SocialLink = () => {
     return (
         <div className="hidden lg:flex flex-col top-[35%] left-0 fixed  z-30">
             <ul>
-                <li className="flex justify-between items-center w-40 h-14 ml-[-100px] hover:rounded-md duration-300 hover:ml-[-10px] my-0"><Link to="https://www.linkedin.com/in/abrar-saleheen-6a9416177/" className="flex justify-between items-center w-full bg-gray-500 py-2 px-4" ><>LinkedIn <FaLinkedin size={30}></FaLinkedin> </></Link></li>
+                <li className="flex justify-between items-center w-40 h-14 ml-[-100px] hover:rounded-md duration-300 hover:ml-[-10px] my-0"><Link to="https://www.linkedin.com/in/abrar-saleheen-6a9416177/" target="_blank" className="flex justify-between items-center w-full bg-gray-500 py-2 px-4" ><>LinkedIn <FaLinkedin size={30}></FaLinkedin> </></Link></li>
                 {/* Github */}
-                <li className="flex justify-between items-center w-40 h-14 ml-[-100px] hover:rounded-md duration-300 hover:ml-[-10px] my-0"><Link to = "https://github.com/saleheen22"  className="flex justify-between items-center w-full bg-gray-500 py-2 px-4" ><>Github <FaGithub size={30}></FaGithub> </></Link></li>
+                <li className="flex justify-between items-center w-40 h-14 ml-[-100px] hover:rounded-md duration-300 hover:ml-[-10px] my-0"><Link to = "https://github.com/saleheen22" target="_blank" className="flex justify-between items-center w-full bg-gray-500 py-2 px-4" ><>Github <FaGithub size={30}></FaGithub> </></Link></li>
                 <li className="flex justify-between items-center w-80 h-14 ml-[-260px] hover:rounded-md duration-300 hover:ml-[-10px] "><span to="mailto: muntasaleheen@gmail.com" className="flex justify-between items-center w-full bg-gray-500 py-2 px-4" ><>muntasaleheen@gmail.com<HiOutlineMail size={30}></HiOutlineMail> </></span></li>
             </ul>
         </div>
