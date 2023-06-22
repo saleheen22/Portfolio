fc4df059d66691e72f38aae4de9011c723dd94bf
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2023-06-22 12:32:26 +0600

    resume updated

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -2,28 +2,13 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import Home from "../Home/Home";
-// import About from "../components/About/About";
-// import Resume from "../components/Resume/Resume";
-// import Contact from "../components/Contact/Contact";
+
 
 
  export  const router = createBrowserRouter([
     {
       path: "/",
       element: <Home></Home>,
-      // children: [
-      //   {
-      //     path: 'about',
-      //     element: <About></About>
-      //   },
-      //   {
-      //     path: 'resume',
-      //     element: <Resume></Resume>
-      //   },
-      //   {
-      //     path: 'contact',
-      //     element: <Contact></Contact>
-      //   }
-      // ]
+
     },
   ]);
